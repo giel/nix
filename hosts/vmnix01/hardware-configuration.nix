82dbbde99271045f47cb73050eb6dacd5e044e4f
Giel (lent480) <gscharff@gisaff.nl>
2024-02-10 08:48:51 +0100

    vmnix01 formatting

--- hosts/vmnix01/hardware-configuration.nix
+++ hosts/vmnix01/hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/profiles/qemu-guest.nix")
+    [
+      (modulesPath + "/profiles/qemu-guest.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
@@ -14,7 +15,8 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/e0d6665b-9c87-4474-abbe-70c06f3430fe";
+    {
+      device = "/dev/disk/by-uuid/e0d6665b-9c87-4474-abbe-70c06f3430fe";
       fsType = "ext4";
     };
 
