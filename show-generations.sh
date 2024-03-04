383e96c67b87f1682a80139bd1a7234020eb5c14
Giel (lent480) <gscharff@gisaff.nl>
2024-03-04 16:13:51 +0100

    show generation newest on top

--- show-generations.sh
+++ show-generations.sh
@@ -1,7 +1,7 @@
 # do nothing but start with sudo
 sudo echo ==========================================================================
 nr=$(sudo nix-env -p /nix/var/nix/profiles/system --list-generations | wc -l)
-sudo nix-env -p /nix/var/nix/profiles/system --list-generations
+sudo nix-env -p /nix/var/nix/profiles/system --list-generations | sort -r
 sudo echo "----(+"
 echo "  $nr generations"
 sudo echo ==========================================================================
