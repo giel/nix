22021cbf354f2b12486aa0220d6b26e82a63ab02
Giel (lent480) <gscharff@gisaff.nl>
2024-05-12 08:36:50 +0200

    nicer show generations

--- show-generations.sh
+++ show-generations.sh
@@ -1,7 +1,13 @@
-# do nothing but start with sudo
-sudo echo ==========================================================================
-nr=$(sudo nix-env -p /nix/var/nix/profiles/system --list-generations | wc -l)
-sudo nix-env -p /nix/var/nix/profiles/system --list-generations | sort -r
-sudo echo "----(+"
-echo "  $nr generations"
-sudo echo ==========================================================================
+echo ------------------------------------------------------------------
+nr=$(nixos-rebuild list-generations | wc -l)
+nixos-rebuild list-generations | sort -r |
+	awk 'NR==1 {printf "%-12s %-21s %-24s %-10s\n", $1, $2, $3" "$4 ,$5}
+NR > 1 {
+    if ($2 == "current")
+        printf "%-12s %-21s %-24s %-10s\n", $1" "$2, $3" "$4, $5, $6;
+    else
+        printf "%-12s %-21s %-24s %-10s\n", $1, $2" "$3, $4, $5;
+}'
+echo "----(+"
+echo "  $((nr - 1)) generations"
+echo ------------------------------------------------------------------
