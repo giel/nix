0814d5ed6878212fc6efa3412a9aee671df19ad2
Giel (lenx1ext) <gscharff@gisaff.nl>
2024-12-29 15:13:55 +0100

    lenx1ext new install first flake version

--- hosts/lenx1ext/boot.nix
+++ hosts/lenx1ext/boot.nix
@@ -1,12 +1,10 @@
 { ... }:
 
 {
-
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Setup keyfile
-  boot.initrd.secrets = { "/crypto_keyfile.bin" = null; };
+  boot.initrd.luks.devices."luks-96e98e38-b1ce-40d5-a50a-ab7c2b55b382".device = "/dev/disk/by-uuid/96e98e38-b1ce-40d5-a50a-ab7c2b55b382";
 
 }
--- hosts/lenx1ext/configuration.nix
+++ hosts/lenx1ext/configuration.nix
@@ -9,19 +9,19 @@
     ../../modules/mixins/openssh.nix
     ../../modules/mixins/sound_pipewire.nix
     ../../modules/mixins/xserver_keyboard_touchpad.nix
-    ../../modules/mixins/graphics-tools.nix
+#    ../../modules/mixins/graphics-tools.nix
 
     ../../modules/packages/system_minimal.nix
     ../../modules/packages/user_minimal.nix
     ../../modules/packages/user_minimal_gui.nix
-    ../../modules/packages/neovim.nix
+#    ../../modules/packages/neovim.nix
     ../../modules/packages/keyboard_firmware.nix
 
     ../../modules/packages/develop.nix
     ../../modules/packages/develop_go.nix
     ../../modules/packages/develop_csharp.nix
     # ../../modules/packages/vm.nix
-    ../../modules/packages/vm_incus.nix
+ #   ../../modules/packages/vm_incus.nix
 
     ../../modules/packages/work.nix
 
