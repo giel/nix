667403b54c3809d36f3ac437aec877792a27d54a
giel (fw1625lin) <mgsch@mac.com>
2025-12-26 10:33:53 +0100

    cleanup fw1625lin configuration

--- hosts/fw1625lin/configuration.nix
+++ hosts/fw1625lin/configuration.nix
@@ -54,8 +54,7 @@
     # Enable networking
     networking.networkmanager.enable = true;
 
-    # Set your time zone.
-    time.timeZone = "Europe/Amsterdam";
+    time.timeZone = userSettings.timeZone;
 
     # Select internationalisation properties.
     i18n.defaultLocale = "en_GB.UTF-8";
@@ -77,8 +76,8 @@
     services.xserver.enable = true;
 
     # Enable the KDE Plasma Desktop Environment.
-    services.displayManager.sddm.enable = true;
-    services.desktopManager.plasma6.enable = true;
+    # services.displayManager.sddm.enable
+    # services.desktopManager.plasma6.enable = true;
 
     # Configure keymap in X11
     services.xserver.xkb = {
@@ -89,34 +88,16 @@
     # Enable CUPS to print documents.
     services.printing.enable = true;
 
-    # Enable sound with pipewire.
-    services.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-
-      # use the example session manager (no others are packaged yet so this is enabled by default,
-      # no need to redefine it in your config for now)
-      #media-session.enable = true;
-    };
 
     # Enable touchpad support (enabled default in most desktopManager).
     # services.xserver.libinput.enable = true;
 
     # Define a user account. Don't forget to set a password with ‘passwd’.
-    users.users.giels = {
+    users.users.${userSettings.user} = {
       isNormalUser = true;
-      description = "Giel Scharff";
+      description = userSettings.userName;
       extraGroups = [ "networkmanager" "wheel" ];
-      packages = with pkgs; [
-        kdePackages.kate
-        #  thunderbird
-      ];
+      shell = pkgs.zsh;
     };
 
     # Install firefox.
@@ -128,32 +109,7 @@
     # List packages installed in system profile. To search, run:
     # $ nix search wget
     environment.systemPackages = with pkgs; [
-      delta
-      git
-      git-lfs
-      neovim
-      vim
-      wget
     ];
-
-    # Some programs need SUID wrappers, can be configured further or are
-    # started in user sessions.
-    # programs.mtr.enable = true;
-    # programs.gnupg.agent = {
-    #   enable = true;
-    #   enableSSHSupport = true;
-    # };
-
-    # List services that you want to enable:
-
-    # Enable the OpenSSH daemon.
-    # services.openssh.enable = true;
-
-    # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
 
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
